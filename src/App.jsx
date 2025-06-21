12153ceb72d73c94953a63f510024728829bb878
Asiful Ahsan <asifulahsan@outlook.com>
2025-06-21 21:05:53 +0600

    Adjusting Body Responsiveness....

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <h1 className="flex justify-center mt-4 font-medium text-4xl">
         React Weather
       </h1>
-      <div className="grid grid-cols-1 justify-center items-center min-h-screen sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8 m-2">
+      <div className="grid grid-cols-1 justify-center items-center min-h-screen sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 m-2">
         <WeatherCard />
         
       </div>
